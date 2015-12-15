e39c1c33c8385158ef3308ef2659fa218585b090
Chris Hinsley <chris.hinsley@gmail.com>
2015-12-15 18:52:09 +0000

    Let glfw3 include the gl headers.

--- view.cpp
+++ view.cpp
@@ -1,7 +1,6 @@
-#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#define GLFW_INCLUDE_GLCOREARB
 #include "router.h"
 #include "GLFW/glfw3.h"
-#include <OpenGL/gl3.h>
 #include <limits>
 #include <iostream>
 #include <fstream>
